cb368cdd06d09086ab9a6b7e4821836cc4c09d0e
Daniel Inoa <danielinoa@gmail.com>
2021-10-29 20:37:55 -0700

    Move attributes to own file

--- Sources/Pin/Extensions/NSLayoutConstraint+Array.swift
+++ Sources/Pin/Extensions/NSLayoutConstraint+Array.swift
@@ -18,26 +18,3 @@ extension Array where Element == NSLayoutConstraint {
         return self
     }
 }
-
-extension Array where Element == NSLayoutConstraint.Attribute {
-
-    /// The width and height of the object’s alignment rectangle.
-    public static var size: Self {
-        [.width, .height]
-    }
-
-    /// The center along the both the x-axis and y-axis of the object’s alignment rectangle.
-    public static var center: Self {
-        [.centerX, .centerY]
-    }
-
-    /// The leading, trailing, top, and bottom edges of the object's alignment rectangle.
-    public static var edges: Self {
-        [.leading, .trailing, .top, .bottom]
-    }
-
-    /// The leadingMargin, trailingMargin, topMargin, and bottomMargin edges of the object's alignment rectangle.
-    public static var marginEdges: Self {
-        [.leadingMargin, .trailingMargin, .topMargin, .bottomMargin]
-    }
-}
