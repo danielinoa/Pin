42dc21609a33fe374cbe434c9ec122cf4a4c578d
Daniel Inoa <danielinoa@gmail.com>
2022-06-21 10:54:16 -0700

    Improve activation strategy

--- Sources/Pin/Core/Pinnable+Activation.swift
+++ Sources/Pin/Core/Pinnable+Activation.swift
@@ -6,22 +6,36 @@
 
 extension Pinnable {
 
-    /// Resolves and activates all the constraints under this tree.
+    /// Resolves and activates all the constraints under this root node.
     @discardableResult
     public func activate() -> Pinnable {
+        // Since it's possible to create a constraint with 2 views at different places in the tree, then there is the
+        // possibility that activating said constraint will trigger an exception. This can occur if one of the views
+        // is being referenced during the activation of a constraint but it has not yet been added as a subview under
+        // their common ancestor.
+        // To prevent this issue all views are added to the view-hierarchy first, and only after are the nodes resolved.
+        composeViewTree()
+        resolveResolvables()
+        return self
+    }
 
-        // Activate children's super-resolvables
+    private func composeViewTree() {
         children.forEach { child in
             child.view.translatesAutoresizingMaskIntoConstraints = false
             (self as? BasePinnable)?.containmentStrategy(child)
+            child.composeViewTree()
+        }
+    }
+
+    private func resolveResolvables() {
+        // Activate children's super-resolvables
+        children.forEach { child in
             child.superResolvables.resolve(with: view)
-            child.activate()
+            child.resolveResolvables()
         }
 
         // Activate self-resolvables
         selfResolvables.resolve()
-
-        return self
     }
 
     /// Reverts and deactivates all the constraints under this tree.
