2d8f140947a8d994e8cb646ab33fd456b4eb352a
Daniel Inoa <danielinoa@gmail.com>
2022-06-21 10:46:03 -0700

    Add relation to sizing helpers

--- Sources/Pin/Extensions/Resolvables/Pinnable+Size.swift
+++ Sources/Pin/Extensions/Resolvables/Pinnable+Size.swift
@@ -27,9 +27,38 @@ extension Pinnable {
     }
 
     /// Sizes the view to the specified height.
-    public func size(height: CGFloat, priority: UILayoutPriority = .required) -> Pinnable {
+    public func size(
+        height: CGFloat, relation: NSLayoutConstraint.Relation = .equal, priority: UILayoutPriority = .required
+    ) -> Pinnable {
         appending(
-            view.heightAnchor.constraint(equalToConstant: height).setPriority(priority)
+            size(attribute: .height, relation: relation, priority: priority, constant: height)
         )
+    }
+
+    /// Sizes the view to the specified height.
+    public func size(
+        width: CGFloat, relation: NSLayoutConstraint.Relation = .equal, priority: UILayoutPriority = .required
+    ) -> Pinnable {
+        appending(
+            size(attribute: .width, relation: relation, priority: priority, constant: width)
+        )
+    }
+
+    private func size(
+        attribute: NSLayoutConstraint.Attribute,
+        relation: NSLayoutConstraint.Relation = .equal,
+        priority: UILayoutPriority = .required,
+        constant: CGFloat
+    ) -> NSLayoutConstraint {
+        .init(
+            item: view,
+            attribute: attribute,
+            relatedBy: relation,
+            toItem: nil,
+            attribute: .notAnAttribute,
+            multiplier: 1,
+            constant: constant
+        )
+        .setPriority(priority)
     }
 }
